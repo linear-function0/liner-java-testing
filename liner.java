b2ea868b961168170f3b3e706e22776e4911fcdc
Linear <linnea7341@outlook.com>
2025-10-13 20:45:51 -0400

    Create liner.java
    created file and put some code in the file twin

---
+++ liner.java
@@ -0,0 +1,9 @@
+public class Liner
+{
+  public static void main(String[]args)
+  {
+    String uhh = "woah its java";
+    System.out.println(uhh);
+    // woah look at that i made a variable and hopefully printed it??? idk twin but this might be tuff
+  }
+}
