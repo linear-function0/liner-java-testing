c63ae147652ea30e683741862ce3785159b4a049
Linear <linnea7341@outlook.com>
2025-10-13 20:58:52 -0400

    Update liner.java
    added variables for some reason??? idk help help help help help help

--- liner.java
+++ liner.java
@@ -5,6 +5,16 @@ public class Liner
     String uhh = "woah its java";
     System.out.println(uhh);
     // woah look at that i made a variable and hopefully printed it??? idk twin but this might be tuff
+      public static void Variabe(String[]args)
+      {
+        String username_probably_maybe = "Linear-function0";
+        int six_or_seven = 67;
+        // sorry for the 67 im so sorry bro im sorry help me please sorry help help help help
+        float decimals_maybe = 473.83;
+        char letter = "B";
+        boolean yes_no_maybe_so = true;
+        // woah its variables twin
+      }
   }
 }
 /* 
