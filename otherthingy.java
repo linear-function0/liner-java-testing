2dcc9a145e9aeedbf0ce28fefdc0767382f1feb6
Linear <linnea7341@outlook.com>
2025-10-14 22:21:13 -0500

    Update otherthingy.java
    me when idk how to code

--- otherthingy.java
+++ otherthingy.java
@@ -1,1 +1,7 @@
-
+public class otherthingy {
+  var username = "Linear-function0"; // variable for my username.
+    public static void otherthingy(String[]args) {
+      otherthingy myObj = new otherThingy();
+      System.out.println(myObj.username); // i feel like this has atleast 6 or 7 errors
+    }
+}
