2e78e741dc4d49bee806c2863df756fa1f0949dc
Linear <linnea7341@outlook.com>
2025-10-14 21:57:51 -0500

    Update liner.java
    added a comment and fixed the errors i think i had

--- liner.java
+++ liner.java
@@ -5,16 +5,15 @@ public class Liner
     String uhh = "woah its java";
     System.out.println(uhh);
     // woah look at that i made a variable and hopefully printed it??? idk twin but this might be tuff
-      public static void main(String[]args)
-        // no idea if this works but whatever idc anyways bro 
-      {
-        String username_probably_maybe = "Linear-function0";
-        int six_or_seven = 67;
+        // no idea if this works but whatever idc anyways bro
+    String username_probably_maybe = "Linear-function0";
+    int six_or_seven = 67;
         // sorry for the 67 im so sorry bro im sorry help me please sorry help help help help
-        float decimals_maybe = 473.83;
-        char letter = "B";
-        boolean yes_no_maybe_so = true;
+    float decimals_maybe = 473.83;
+    char letter = "B";
+    boolean yes_no_maybe_so = true;
         // woah its variables twin
+    // ts pmo bro
       }
   }
   // please help it has errors im gonna cry im gonna cry help me help me help me :((((((
